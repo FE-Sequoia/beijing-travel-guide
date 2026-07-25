e8040482b81c8623944be8cca98f3ce80c83e0fe
sequoia tungfang <sequoia.tungfang@gmail.com>
2026-07-25 19:52:00 +0800

    feat: 在攻略页添加智能路线规划入口卡片
    - 在攻略列表顶部添加突出的路线规划入口卡片
    - 卡片采用渐变色背景，显示支持的行程天数标签
    - 点击跳转到 itineraries 页面
    - 添加分割线标题区分路线规划和攻略指南区域

--- beijing-travel-mp/pages/guides/index.js
+++ beijing-travel-mp/pages/guides/index.js
@@ -1,2 +1,7 @@
 const { getGuides } = require('../../utils/data');
-Page({ data: { guides: [] }, onShow() { this.setData({ guides: getGuides() }); }, onGuide(e) { wx.navigateTo({ url: '/pages/guide-detail/index?id=' + e.currentTarget.dataset.id }); } });
+Page({
+  data: { guides: [] },
+  onShow() { this.setData({ guides: getGuides() }); },
+  onGuide(e) { wx.navigateTo({ url: '/pages/guide-detail/index?id=' + e.currentTarget.dataset.id }); },
+  onItineraryEntry() { wx.navigateTo({ url: '/pages/itineraries/index' }); }
+});
